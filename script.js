4d237e8bab26830a25f3390564b6d6fe976999af
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-09 14:25:15 +0600

    added const]

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const main = document.querySelector('main'); 
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function displayList(){
+  main.innerHTML = "";
   list.forEach(item => {
    const div =  document.createElement('div');
    div.textContent = item;
    main.append(div);
   });
 }
+displayList();
 displayList();
